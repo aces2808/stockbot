67a645b950f6aaa0a24b8b4db5e3bbbb0534290b
Jomer Pangilinan <aces28@Jomers-MacBook-Pro.local>
2021-07-20 17:12:19 +0800

    [crypto] Add requirements for single dependency

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# stockbot
+# Stock Bot (Binance)
+
+Export API details in environment
+
+```
+export BNB_API_KEY=<value>
+export BNB_SEC_KEY=<value>
+```
+
+Run test
+
+```
+$pytest -s
+```
--- test/test_model.py
+++ test/test_model.py
@@ -1,6 +1,7 @@
 import unittest
 import requests
 import os
+import logging
 
 
 class TestModel(unittest.TestCase):
@@ -14,4 +15,8 @@ class TestModel(unittest.TestCase):
 
     def test_bnb_details(self):
         bnb_api_key = os.environ.get('BNB_API_KEY')
-        bnb_api_sec = os.environ.get('BNB_API_SEC')
+        bnb_api_sec = os.environ.get('BNB_SEC_KEY')
+        self.assertIsNotNone(bnb_api_key)
+        self.assertIsNotNone(bnb_api_sec)
+        print(f"BNB API key : {bnb_api_key}")
+        print(f"BNB API key : {bnb_api_sec}")
